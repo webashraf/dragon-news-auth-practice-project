fa4da0939a14ea25384679ba5624f59c3b4b8f25
webashraf <aliashraftamim@gmail.com>
2023-04-18 12:51:20 +0600

    add google and github button

--- src/PAGES/SHARED/Header/Header.jsx
+++ src/PAGES/SHARED/Header/Header.jsx
@@ -1,8 +1,12 @@
 import moment from 'moment';
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
 import Marquee from "react-fast-marquee";
 import logo from '../../../assets/logo.png';
-import { Container } from 'react-bootstrap';
+
 
 
 const Header = () => {
@@ -13,9 +17,29 @@ const Header = () => {
                 <p>Journalism Without Fear or Favour</p>
                 <p>{moment().format('dddd, MMMM D, YYYY')}</p>
                 <div className='d-flex'>
-                    <button className='btn btn-danger'>Latest</button>            
+                    <button className='btn btn-danger'>Latest</button>
                     <Marquee speed={100} pauseOnHover={true}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore recusandae ipsum similique praesentium! Explicabo error atque ab temporibus, beatae voluptatum!</Marquee>
                 </div>
+            </div>
+            <div>
+                <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                    <Container>
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="mx-auto">
+                                <Nav.Link href="#features">Home</Nav.Link>
+                                <Nav.Link href="#pricing">About</Nav.Link>
+                                <Nav.Link href="#pricing">Carear</Nav.Link>
+                            </Nav>
+                            <Nav>
+                                <Nav.Link href="#deets">More deets</Nav.Link>
+                                <Nav.Link eventKey={2} href="#memes">
+                                <Button variant="secondary">Log In</Button>                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
             </div>
         </Container>
     );
--- src/PAGES/SHARED/RightNav/RightNav.jsx
+++ src/PAGES/SHARED/RightNav/RightNav.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
             <h1>Right Nav</h1>
+            <Button className='mb-2' variant="outline-dark" size="lg">  <FaGoogle /> Log in with google</Button>
+            <Button variant="outline-secondary" size="lg"><FaGithub /> Log in with github</Button>
         </div>
     );
 };
