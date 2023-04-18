0b2f182cd2839898ab7b14670cdb3781d7d229f0
webashraf <aliashraftamim@gmail.com>
2023-04-18 11:48:11 +0600

    add marque on header

--- src/PAGES/SHARED/Header/Header.jsx
+++ src/PAGES/SHARED/Header/Header.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import logo from '../../../assets/logo.png';
 import moment from 'moment';
+import React from 'react';
+import Marquee from "react-fast-marquee";
+import logo from '../../../assets/logo.png';
+import { Container } from 'react-bootstrap';
 
 
 const Header = () => {
     return (
-        <div className='py-5'>
+        <Container className='py-5'>
             <div className='text-center'>
                 <img src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
-            <p>{moment().format('dddd, MMMM D, YYYY')}</p>
+                <p>Journalism Without Fear or Favour</p>
+                <p>{moment().format('dddd, MMMM D, YYYY')}</p>
+                <div className='d-flex'>
+                    <button className='btn btn-danger'>Latest</button>            
+                    <Marquee speed={100} pauseOnHover={true}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore recusandae ipsum similique praesentium! Explicabo error atque ab temporibus, beatae voluptatum!</Marquee>
+                </div>
             </div>
-        </div>
+        </Container>
     );
 };
 
