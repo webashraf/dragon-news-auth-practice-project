a51d60a6ca9404fc629cc2460d49d5babe866cbc
webashraf <aliashraftamim@gmail.com>
2023-04-18 16:46:23 +0600

    solve server problem

--- dragon-backend-server/index.js
+++ dragon-backend-server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require("cors");
+var cors = require('cors');
 const categories = require("./categoriesData/categories.json");
 
+app.use(cors())
 
-
-app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello world I'm backend sever");
 });
--- src/PAGES/SHARED/LeftNav/LeftNav.jsx
+++ src/PAGES/SHARED/LeftNav/LeftNav.jsx
@@ -7,7 +7,7 @@ const LeftNav = () => {
         fetch("http://localhost:5000/categories")
         .then(res=> res.json())
         .then(data=> console.log(data))
-        .catch(err => console.log(err))
+        // .catch(err => console.log(err))
     },[])
 
 
